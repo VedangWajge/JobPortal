df79f7a784a647f3eb40e32f4667a31d149f03a8
VedangWajge <vedangwajge2004@gmail.com>
2024-09-17 11:53:56 +0530

    updates

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,121 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+    try{
+        const {fullname, email, phoneNumber, password , role} = req.body;
+        if(!fullname || !email || !phoneNumber || !password || !role){
+            return res.status(400).json({
+                message : "All fields are required",
+                success : false
+            });
+        };
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({
+                message : "User already exists",
+                success : false
+            })
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        await User.create({
+            fullname, 
+            email, 
+            phoneNumber,
+            password : hashedPassword,
+            role
+        })
+
+        return res.status(201).json({
+            message : "Account created successfully",
+            success : true
+        })
+    }catch(error){
+        console.log(error);
+    }
+};
+
+export const login = async(req, res) => {
+    try{
+        const {email, password} = req.body;
+        if(!email || !password || !role){
+            return res.status(400).json({
+                message : "All fields are required",
+                success : false
+            });
+        };
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                message : "Incorrect email or password",
+                success : false
+            })
+        };
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                message : "Incorrect email or password",
+                success : false
+            })
+        };
+        //check role is correct or not
+        if(role !== user.role){
+            return res.status(400).json({
+                message : "Account does not exist with current role",
+                success : false
+            })
+        };
+
+        const tokenData = {
+            userId : user._id
+        }
+
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET, {expiresIn : "1d"}); 
+
+        uesr = {
+            _id : user._id,
+            fullname : user.fullname,
+            email : user.email,
+            phoneNumber : user.phoneNumber,
+            role : user.role,
+            profile : user.profile
+        }
+
+
+        return res.status(200).cookie("token", token, {maxAge : 1*24*60*60*1000, httpsOnly:true, sameSite : 'strict'}).json({
+            message : `Welcome back ${user.fullname}`,
+            user, 
+            success : true
+        })
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const logout = async(req, res) => {
+    try{
+        return res.status(200).cookie("token", "", {maxAge:0}).json({
+            message : "Logged out successfully.",
+            success : true
+        })
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const updateProfile = async(req, res) => {
+    try {
+        const {fullname, email, phoneNumber, bio, skills} = req.body;
+        if(!fullname || !email || !phoneNumber || !bio || !skills){
+            return res.status(400).json({
+                message : "All fields are required",
+                success : false
+            });
+        };
+    } catch (error) {
+        console.log(error);
+    }
+}
+    
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,4 +5,38 @@ const userSchema = new mongoose.Schema({
         required : true
     },
     
-})
+    email :{
+        type : String,
+        required : true,
+        unique : true
+    },
+
+    phoneNumber :{
+        type : Number,
+        required : true
+    },
+
+    email :{
+        type : String,
+        required : true
+    },
+    role :{
+        type: String,
+        enum:['student', 'recruiter'],
+        required:true
+    },
+    profile :{
+        bio : {type:String},
+        skills : {type:String},
+        resume : {type:String}, // Url to resume file
+        resumeOriginalName : {type:String},
+        company : {type:mongoose.Schema.Types.ObjectId, ref:'Company'}, 
+        profilePhoto : {
+            type:String,
+            default : ""
+        }     
+    },
+
+}, {timestamps:true});
+
+export const User = mongoose.model('User', userSchema);
