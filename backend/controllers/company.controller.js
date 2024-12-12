b395171906897086bcd2c7598f6a4ad6971a406b
VedangWajge <vedangwajge2004@gmail.com>
2024-12-12 15:35:10 +0530

    afternoon

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,5 +1,5 @@
 import {Company} from "../models/company.model.js"
-export const regsiterCompany = async (req, res) => {
+export const registerCompany = async (req, res) => {
     try {
         const {companyName} = req.body;
         if(!companyName){
@@ -33,13 +33,61 @@ export const regsiterCompany = async (req, res) => {
 export const getCompany = async (req, res) => {
     try {
         const userId = req.id;
-        const companies = await Company.find(userId);
+        const companies = await Company.find({userId});
         if(!companies){
             return res.status(404).json({
                 message : "No companies found",
                 success : false
             })
         }
+        return res.status(200).json({
+            companies,
+            success : true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getCompanyById = async (req, res) => {
+    try {
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if(!company){
+            return res.status(404).json({
+                message : "Company not found",
+                success : false
+            })
+        }
+        return res.status(200).json({
+            company,
+            success : true  
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updateCompany = async (req, res) => {
+    try {
+        const {name, description, website, location} = req.body; 
+        const file = req.file;
+        // cloudinary ayega
+        
+        const updateData = {name, description, website, location};
+
+        const company = await Company.findByIdAndUpdate(req.params.id, updateData, {new:true});
+        if(!company){
+            return res.status(404).json({
+                message : "Company not found",
+                success : false
+            })
+        } 
+        return res.status(200).json({
+            message : "Company information updated.",
+            success : true
+        })
+
     } catch (error) {
         console.log(error);
     }
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected successfully`);
     } catch (error) {
-        console.log(`Error: `);
+        console.log(`Error: MongoDB Connection Failed with ${error}`);
     }
 }
 
