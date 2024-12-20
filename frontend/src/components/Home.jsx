aeb45d5c0856ae7f997cfb0748f1e8e09aab6aa8
VedangWajge <vedangwajge2004@gmail.com>
2024-12-20 17:16:45 +0530

    update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
+import Footer from './shared/Footer'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <HeroSection />
         <CategoryCarousel />
         <LatestJobs />
-        {/* <Footer /> */}
+        <Footer />
     </div>
   )
 }
