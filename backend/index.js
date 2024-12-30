c45adae193e2d726b2a4c91895ea0e44d94a7bec
VedangWajge <vedangwajge2004@gmail.com>
2024-12-30 11:35:41 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true
-}
+    origin: [
+        "http://localhost:5173", 
+        "https://v2w-jobportal-frontend.vercel.app",
+        "https://jobportal-8kqh.onrender.com"
+    ],
+    credentials: true, // Allow cookies to be sent
+};
+
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
